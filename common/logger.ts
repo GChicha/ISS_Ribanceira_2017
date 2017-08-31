18458f33fe7ff9cdc5c37b383dee3972a4ede9ce
Guilherme Chichanoski <guilherme.chichanoski@gmail.con>
2017-08-30 21:19:07 -0300

    Comitt Inicial

---
+++ common/logger.ts
@@ -0,0 +1,7 @@
+import * as logger from 'winston'
+
+export class Logger extends logger.Logger {
+    constructor(section : string) {
+        super()
+    }
+}
---
+++ server/index.ts
@@ -0,0 +1,15 @@
+import * as express from 'express'
+import * as bodyParser from 'body-parser'
+
+export class App {
+    public app : express.Application
+
+    constructor() {
+        this.app = express()
+
+        this.app.use(bodyParser.json())
+        this.app.use(bodyParser.urlencoded({
+            extended : true
+        }))
+    }
+}
