68b6108ecc3a149bd90ecf7b5815cf58bf47ae1d
Guilherme Chichanoski <guilherme.chichanoski@gmail.con>
2017-09-01 10:54:55 -0300

    [server] inicia com log

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,10 +2,12 @@ import * as express from 'express'
 import * as bodyParser from 'body-parser'
 import { Logger } from '../common/logger'
 import * as logger from 'winston'
+import * as httpLogger from 'morgan'
 
 export class App {
     public app : express.Application
     public logger : Logger = new Logger("App")
+    public port : number = parseInt(<string> process.env.PORT) || 8080
 
     constructor() {
         this.app = express()
@@ -13,10 +15,14 @@ export class App {
         this.config()
 
         this.route()
+
+        this.app.listen(this.port, () => {
+            this.logger.info("Listening on: " + this.port)
+        })
     }
 
     private config() {
-        /* Configuração de middleware */
+        this.app.use(httpLogger('dev'))
         this.app.use(bodyParser.json())
         this.app.use(bodyParser.urlencoded({
            extended : true
