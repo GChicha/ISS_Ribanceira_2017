136868fd7ecb868aebbdc526db361ac0f7ef8c29
Guilherme Chichanoski <guilherme.chichanoski@gmail.con>
2017-10-22 01:30:24 -0200

    Adiciona a rota contratos

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -3,12 +3,14 @@ import { Db } from 'mongodb'
 
 import { Empresas } from '../controladores/empresas'
 import { Sindicatos } from '../controladores/sindicatos'
+import { Contratos } from '../controladores/contratos'
 
 export class ApiRouter {
     private _router : Router = Router()
-    private empresas : Empresas
 
+    private empresas : Empresas
     private sindicatos : Sindicatos
+    private contratos : Contratos
 
     constructor (Db : Db) {
         this._router.get("/", (req, res) => {
@@ -23,6 +25,8 @@ export class ApiRouter {
         this.sindicatos = new Sindicatos(Db)
         this.router.use("/sindicatos", this.sindicatos.router)
 
+        this.contratos = new Contratos(Db)
+        this.router.use("/contratos", this.contratos.router)
     }
 
     get router() : Router {
