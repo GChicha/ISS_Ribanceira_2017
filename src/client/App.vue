bf5388ef0a67958c52e35c6c7b69c8697e64afa8
Guilherme Chichanoski <guilherme.chichanoski@gmail.con>
2017-11-01 20:40:59 -0200

    Adiciona bootstrap

--- src/client/App.vue
+++ src/client/App.vue
@@ -1,24 +1,24 @@
 <template>
-    <div id="app">
+    <div id="app" class="site-wrapper">
         <Navbar></Navbar>
-        <Empresas></Empresas>
+        <router-view></router-view>
     </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar'
-import Empresas from './components/Empresas'
 
 export default {
     name: 'app',
     components: {
-        Navbar,
-        Empresas
+        Navbar
     }
 }
+
+document.body.classList += " bg-secondary"
 </script>
+
+<style src="./assets/bootstrap.css"></style>
 
 <style>
 </style>
-
-<style src="element-ui/lib/theme-default/index.css"></style>
