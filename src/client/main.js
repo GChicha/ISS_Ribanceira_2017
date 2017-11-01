3012467cc4096d1257ce301ad9b8b0f1d10b8323
Guilherme Chichanoski <guilherme.chichanoski@gmail.con>
2017-11-01 20:39:57 -0200

    removes element-ui & add router

--- src/client/main.js
+++ src/client/main.js
@@ -1,10 +1,21 @@
+import './assets/bootstrap'
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App'
-import ElementUI from 'element-ui'
 
-Vue.use(ElementUI)
+Vue.use(VueRouter)
+
+import Empresas from './components/Empresas'
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {path: '/empresas/:id?', component: Empresas}
+    ]
+})
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router: router
 })
